bd17ea0b625bed3e06864c169a026669f66d3a2d
Andrey <uir008@gmail.com>
2025-07-24 17:29:49 +0300

    1

--- main.go
+++ main.go
@@ -2,31 +2,35 @@ package main
 
 import (
 	"fmt"
-	
+	"sync"
 )
+
+func foo(wg *sync.WaitGroup) {
+    defer wg.Done()
+    fmt.Println("hello from foo")
+}
+
 
 func main() {
 
-ch := make(chan int)
+wg := &sync.WaitGroup{}
 
- go say("Hello Go!!!!", ch)
 
-for a := range ch {
-    fmt.Println(a)
+wg.Add(1)
+     go func() {
+        defer wg.Done()
+fmt.Println("hello from goroutine")
+     }()
+
+     wg.Add(1)
+go foo(wg)
+ 
+wg.Wait()
+
+
+fmt.Println("hello from main")
+     
+
 }
-fmt.Println(<-ch)
-fmt.Println(<-ch)
-fmt.Println(<-ch)
-fmt.Println(<-ch)
-}
 
-func say(greet string, ch chan int) {
 
-for i := 0; i <= 5; i++ {
-    fmt.Println(i)
-    ch <- i
-}
-
-close(ch)
-ch <- 1
-}
